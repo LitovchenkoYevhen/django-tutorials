19da018c7c46fbea7eca913fc1561b68ff400c7f
Yevhen Litovchenko <litovchenkoyevhen@gmail.com>
2021-05-11 12:58:48 +0300

    After 14 lessons on YouTube

--- polls/admin.py
+++ polls/admin.py
@@ -5,8 +5,11 @@ from .models import Question, Choice
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'pub_date',)
-    list_display_links= ('question_text', 'pub_date',)
+    list_display = ('question_text', 'pub_date',) # добавляем в админку в список вопросов столбец с датой
+    list_display_links= ('question_text', 'pub_date',) # делаем строки ссылками
+    search_fields = ('question_text', 'pub_date',)
+
+
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
